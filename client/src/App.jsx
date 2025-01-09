9e755e60c6b69cae835d3ed5a7017b241f81e72e
Kskays <kapom0808@gmail.com>
2025-01-09 10:09:56 +0700

    daisytailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <button class="btn btn-xs">Xsmall</button>
+        <button class="btn btn-sm">Small</button>
+        <button class="btn">Medium</button>
+        <button class="btn btn-lg">Large</button>
+        <button class="btn btn-xl">Xlarge</button>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
