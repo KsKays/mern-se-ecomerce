5c6ccdaa4ec57feef00db0227ee6c497731ba911
Kskays <thirayut1379@gmail.com>
2025-03-09 20:13:23 +0700

    UPdate2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import router from "./routers/Router.jsx";
-
 import { RouterProvider } from "react-router";
 import AuthProvider from "./context/Authcontext.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
